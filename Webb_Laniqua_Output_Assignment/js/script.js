183280038a958fc0e5323c05dc8f11de812c8ded
niq414 <laniqua.webb@aim.com>
2015-03-05 20:28:27 -0500

    Outputs
    Created and tested console.log

--- Webb_Laniqua_Output_Assignment/js/script.js
+++ Webb_Laniqua_Output_Assignment/js/script.js
@@ -1,19 +1,30 @@
-//Laniqua Webb - Scalable Data Infrastructure - C201503 Term - Output Assignment
+//Laniqua Webb - March 5th 2015 - Scalable Data Infrastructure - C201503 Term - Output Assignment
 
 
 //Variables
 
 var myGreeting = "Greeting's!"; //This is how the user will be welcomed.
+var whoMe = "about me";
 var thankYou = "my Code"; //Thank you for reading my code
-var myChildren = "2"; //This represents the amount of children I have
-var pets = "false"; // This boolean is false because I do not have any pets
+var myChildren = 2; //This represents the amount of children I have
+var run = true; // This boolean is false because I do not have any pets
 var skillsLearned = "coding skills";
+var method = "learn";
+var thanks = "Thank you, have a great day!";
 
 
 
 console.log(myGreeting);
 console.log("Thank you for viewing " +thankYou+ "!" );
-console.log("I am not only showing off a my " + skillsLearned + ".");
+console.log("I am showing off a my " + skillsLearned + ".");
+console.log("In addition you will get to " +method+ " a little more " +whoMe+ "!");
+console.log("I have a total of " +myChildren+ " young boys.");
+console.log("It is " +run+ " I love to run!" );
+console.log(thanks);
+
+
+
+
 
 
 
