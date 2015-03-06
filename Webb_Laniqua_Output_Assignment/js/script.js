cf9fe4cbb92205a318ad46f00561b2d72752b5af
niq414 <laniqua.webb@aim.com>
2015-03-05 19:33:04 -0500

    Created Variables
    Entered values

---
+++ Webb_Laniqua_Output_Assignment/js/script.js
@@ -0,0 +1,12 @@
+//Laniqua Webb - Scalable Data Infrastructure - C201503 Term - Output Assignment
+
+
+//Variables
+
+var myGreeting = "Greeting's!"; //This is how the user will be welcomed.
+var thankYou = "my Code"; //Thank you for reading my code
+var myChildren = "2"; //This represents the amount of children I have
+var pets = "false"; // This boolean is false because I do not have any pets
+
+
+
