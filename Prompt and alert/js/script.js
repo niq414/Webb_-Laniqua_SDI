e431c6da4c3ece5543a59425a05cf0aaf122619a
niq414 <laniqua.webb@aim.com>
2015-03-09 21:25:54 -0400

    prompt and alert

---
+++ Prompt and alert/js/script.js
@@ -0,0 +1,7 @@
+//Prompt and Alert
+
+var width = prompt("We are calculating the area of a rectangle. Please enter the width.");
+var height = prompt("Please enter the height?");
+
+var area = width * height;
+//console.log(area);
