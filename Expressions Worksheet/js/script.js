eb38abdb89f2c8638d2c574f075840aba264560f
niq414 <laniqua.webb@aim.com>
2015-03-12 20:58:51 -0400

    Slice of Pie

--- Expressions Worksheet/js/script.js
+++ Expressions Worksheet/js/script.js
@@ -12,5 +12,24 @@ var sparkyAge = humanYears * dogYears;
 console.log(sparkyAge);
 alert("Sparky is " +sparkyAge+ " in dog years!");
 
+//Slice of Pie part 1
+
+var pizzaOrdered = 1;
+var slices = 1;
+var people = 1;
+
+var pizzaOrdered = prompt("How many pizzas were ordered?");
+var slices = prompt("How many slices are on each pizza?");
+var people = prompt("How many people are at the party?");
+
+var perPerson = pizzaOrdered * slices / people;
+console.log(perPerson);
+alert("Each person will receive " +perPerson+ " slices of pizza!")
+
+//Slice of pie 2
+
+var leftOver = perPerson%slices
+console.log(leftOver);
+alert("Sparky will have " +leftOver+ " slices of pizza.");
 
 
