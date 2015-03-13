b1e885d843e42c40c576288e1c8475ee9479ecc2
niq414 <laniqua.webb@aim.com>
2015-03-12 20:19:18 -0400

    Dog years

---
+++ Expressions Worksheet/js/script.js
@@ -0,0 +1,16 @@
+//Laniqua Webb Expressions Worksheet March 12 2015
+
+//Dog years
+
+var humanYears = 1;
+var dogYears = 7;
+
+var humanYears = prompt("How old is Sparky?");
+
+var sparkyAge = humanYears * dogYears;
+
+console.log(sparkyAge);
+alert("Sparky is " +sparkyAge+ " in dog years!");
+
+
+
