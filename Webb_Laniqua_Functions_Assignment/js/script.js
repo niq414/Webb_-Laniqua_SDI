7d5318fcbf97b25189ea655635d25b6899d7dd43
niq414 <laniqua.webb@aim.com>
2015-03-26 23:58:23 -0400

    commits

--- Webb_Laniqua_Functions_Assignment/js/script.js
+++ Webb_Laniqua_Functions_Assignment/js/script.js
@@ -1,40 +1,39 @@
 //Laniqua Webb -  Functions Assignment - March 26th 2015
 
 
-
+//Declaring variables
 
 var lotteryType;
 var Florida;
 var power;
 var powerNumber;
 
-var lotteryType = prompt("Hello! Would you like to play the Florida Lottery or the Power Ball Lottery? For Florida type Florida. For Power Ball type Power ball.");
+var lotteryType = prompt("Hello! Would you like to play the Florida Lottery or the Power Ball Lottery? For Florida type Florida. For Power Ball type Power ball.");//prompt to determine which lottery the user would like to select
 
-function lotteryType (selection){
+function lotteryType (selection){//function for lottery type
     var blank = 5;
-    lotteryType = prompt("Please select a Lottery.");
+    lotteryType = prompt("Please select a Lottery.");//prompt if user leaves blank
 
     while(selection === ""){
-        selection = prompt("Please do not leave blank.");
+        selection = prompt("Please do not leave blank.");//prompt for no answer
 
         if(blank = 5){
-            console.log("You did not made a selection.");
-            break;
+            console.log("You did not made a selection.");//if blank then no selection was made
+            break;//end the loop
         }
     }
-    return selection
+    return selection //tell user which selection was made
 }
 
-function randomNumGen(min, max){
-    var lotteryArray = [];
-    var f =0;
+function randomNumGen(min, max){ //created number generator to find random numbers
+    var lotteryArray = []; //created array to hold numbers
 
-    for(var f =0; f < 6; f++);{
-         randomNumGen = Math.random() * (max - min)+ min;
+    for(var f = 0; f < 6; f++);{
+         florida = Math.random() * (max - min)+ min;//
         lotteryArray[f] = Florida;
     }
     return lotteryArray;
 }
-var florida = randomNumGen(1, 53, 5);
+florida = randomNumGen(1, 53, 5);
 
-console.log("The Florida lottery numbers are " +florida[0]+ +florida[1]+ +florida[2]+ +florida[3]+ florida[4]+ ".");
+
